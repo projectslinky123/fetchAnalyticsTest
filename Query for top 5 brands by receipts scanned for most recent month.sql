8ed82f0e87738010fd7361ca8d2de3907911e494
projectslinky123 <70034783+projectslinky123@users.noreply.github.com>
2021-06-02 07:28:22 -0500

    Add files via upload

--- Query for top 5 brands by receipts scanned for most recent month.sql
+++ Query for top 5 brands by receipts scanned for most recent month.sql
@@ -1,12 +1,19 @@
 
+declare @maxDate date = (select max(dateScanned) from receipt )
+declare @startDate date = dateadd(day, 1-day(@maxDate),@maxDate)
+declare @endDate date = dateadd(day, -1, dateadd(month, 1, @startDate))
 
-select top 5 r.dateScanned, numberOfReceipts = count(distinct r.receiptId)
+select top 5 b.brandId, b.name, b.brandCode, numberOfReceipts = count(distinct r.receiptId)
 from receipt r
 	inner join receiptItem ri on ri.receiptId = r.receiptId
 	inner join partnerItem p on ri.partnerItemid = p.partnerItemId
 	inner join item i on i.itemid = p.itemId
-group by r.dateScanned
-order by r.dateScanned
+	inner join brand b on b.brandid = i.brandid
+where dateScanned between @startDate and @enddate
+group by b.brandId, b.name, b.brandCode
+order by 4 desc
+
+/*** Assumed most recent month mean the current partial month and not the most recent full month.  ***/
 
 /**** to ask the user who is goign to use the query that the receipts scanned is really what they want. 
 	since a person can scan the receipt months(assuming) after purchase, which could scew the results based on what analysis they were doing  */ 
